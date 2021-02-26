8c2d9502816d5050a7eb117e436dd77c4a3f3a85
Mason-Edwards <edwardsmason2000@gmail.com>
2021-02-26 16:19:51 +0000

    Changed border colours

--- config.def.h
+++ config.def.h
@@ -1,7 +1,10 @@
 /* See LICENSE file for copyright and license details. */
+/* Includes */
+#include <X11/XF86keysym.h>
+
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx	    = 5;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,10 +16,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+const char normbordercolour[]  = "#FFFFFF";
+const char selbordercolour[]   = "#FF0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, normbordercolour },
+	[SchemeSel]  = { col_gray4, col_cyan,  selbordercolour  },
 };
 
 /* tagging */
@@ -35,7 +41,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -58,7 +64,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", NULL }; /* Default Terminal */
+static const char *mutecmd[] = {"amixer", "-q", "set", "Master", "toggle", NULL};
+static const char *volupcmd[] = {"amixer", "-q", "set", "Master", "5%+", "unmute", NULL};
+static const char *voldowncmd[] = {"amixer", "-q", "set", "Master", "5%-", "unmute", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -88,6 +97,9 @@ static Key keys[] = {
 	{ MODKEY,			XK_minus,  setgaps,	   {.i = -1 } },		
 	{ MODKEY,			XK_equal,  setgaps,	   {.i = +1 } },		
 	{ MODKEY|ShiftMask,		XK_equal,  setgaps,	   {.i =  0 } },		
+	{0,				XF86XK_AudioMute, 	 spawn,	   {.v = mutecmd } }, 
+	{0,				XF86XK_AudioLowerVolume, spawn,	   {.v = voldowncmd } }, 
+	{0,				XF86XK_AudioRaiseVolume, spawn,	   {.v = volupcmd } }, 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
